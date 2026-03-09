45e1d2ff5d502b3846dabd64b4ee60ca71726322
michaela <info.michaela.lm@gmail.com>
2026-03-09 10:36:13 +0800

    Updated reports.py
    Updates made:
    (1) Moved data proccessing to report_email.py
    (2) Removed unnecessary module imports
    (3) Added code documentation
    (4) Code Cleanup

--- reports.py
+++ reports.py
@@ -1,35 +1,26 @@
 #!/usr/bin/env python3
 
+"""
+NAME : reports.py
 
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, Image
+FUNCTIONALITY:
+  > Generates PDF reports
+
+HOW TO USE:
+1. Import as a module
+2. Report created will be saved in current working directory or filepath used in "attachment"
+  ** note: existing report will be overwritten
+"""
+
+from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib import colors
-from reportlab.graphics.shapes import Drawing
-from reportlab.graphics.charts.piecharts import Pie
-from datetime import date
-from pathlib import Path
 
+def generate_report(attachment: str , report_title: str, paragraph: str) -> None:
 
-def generate_report(src_dir):
-
-	fruit_report = SimpleDocTemplate("processed.pdf")
+	fruit_report = SimpleDocTemplate(filename=attachment)
 	report_style = getSampleStyleSheet()
 
-	report_title = Paragraph(f"Processed Updated on {date.today():%B %d, %Y}", report_style["h1"])
-	
-	fruit_data = []
+	title = Paragraph(text=report_title, style=report_style["h1"])
+	report_data = Paragraph(text=paragraph, style=report_style["BodyText"])
 
-	for fruit_item in Path(src_dir).iterdir():
-		with open(fruit_item, "r") as txt_file:
-			fruit = txt_file.readlines()
-			fruit_data.append("<br/>")
-			fruit_data.append(f"name: {fruit[0].rstrip()}")
-			fruit_data.append(f"weight: {fruit[1].rstrip()}")
-
-
-	report_data = Paragraph("<br/>".join(fruit_data))
-	
-	fruit_report.build([report_title, report_data])
-
-if __name__ == "__main__":
-		generate_report("supplier-data/descriptions/")
+	fruit_report.build(flowables=[title, report_data])
