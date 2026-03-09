aa2a89eaf0ce97a70cab15d551778db15da3bb85
michaela <info.michaela.lm@gmail.com>
2026-03-09 10:30:37 +0800

    Updated emails.py
    Updates made:
    (1) Added code documentation
    (2) Removed redundant function generate_email_error() and
    (3) Modified generate_email() to handle emails with no attachments
    (4) Code cleanup

--- emails.py
+++ emails.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env python3
 
+"""
+NAME : emails.py
+
+FUNCTIONALITY:
+  > Generates and sends email (with or without attachment)
+
+HOW TO USE:
+1. Import as a module
+2. Call generate_email first to build email body (with or without attachment)
+3. Call send_email to send email
+"""
+
 from email.message import EmailMessage
 import mimetypes
 import smtplib
 import os
 
-
-def generate_email(sender, recepient, subject, email_body, attachment):
+def generate_email(sender:str, recipient:str, subject:str, body:str, attachment:str) -> EmailMessage:
 	email_message = EmailMessage()
 
 	email_message["From"] = sender
-	email_message["To"] = recepient
+	email_message["To"] = recipient
 	email_message["Subject"] = subject
-	email_message.set_content(email_body)
+	email_message.set_content(body)
 
-	mime_type, _ = mimetypes.guess_type(attachment)
-	attach_filename = os.path.basename(attachment)
-	mimetype, subtype = mime_type.split("/", 1)
+	try:
+		if attachment is not None:
+			mime_type, _ = mimetypes.guess_type(attachment)
+			attach_filename = os.path.basename(attachment)
+			mimetype, subtype = mime_type.split("/", 1)
 
-	with open(attachment, "rb") as attach_file:
-		email_message.add_attachment(attach_file.read(), maintype = mimetype, subtype=subtype, filename=attach_filename)
-
+			with open(attachment, "rb") as attach_file:
+				email_message.add_attachment(attach_file.read(), maintype = mimetype, subtype=subtype, filename=attach_filename)
+	except Exception as e:
+		print(f"\nError encountered at generate_email(): {e}")
 	return email_message
 
-def generate_email_error(sender, recepient, subject, email_body):
-	email_message = EmailMessage()
-
-	email_message["From"] = sender
-	email_message["To"] = recepient
-	email_message["Subject"] = subject
-	email_message.set_content(email_body)
-
-	return email_message
-
-def send_email(message):
-	mail_server = smtplib.SMTP("localhost") 
-	mail_server.send_message(message)
-	mail_server.quit()
+def send_email(message: EmailMessage):
+	try:
+		mail_server = smtplib.SMTP("localhost") 
+		mail_server.send_message(message)
+		mail_server.quit()
+	except Exception as e:
+		print(f"\nError encountered at send_email(): {e}")
 
